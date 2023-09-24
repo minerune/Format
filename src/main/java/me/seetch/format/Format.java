7a0ae0c1f20c070e6d4c9266b9eeb6a3298792ce
Mikhail Sergeevich <seetch@inbox.ru>
2023-09-24 05:39:44 +0300

    Fixes

--- src/main/java/me/seetch/format/Format.java
+++ src/main/java/me/seetch/format/Format.java
@@ -4,42 +4,12 @@ import java.util.List;
 
 public enum Format {
 
-    BLACK("§0"),
-    DARK_BLUE("§1"),
-    DARK_GREEN("§2"),
-    DARK_AQUA("§3"),
-    DARK_RED("§4"),
-    DARK_PURPLE("§5"),
-    GOLD("§6"),
-    GRAY("§7"),
-    DARK_GRAY("§8"),
-    BLUE("§9"),
-    GREEN("§a"),
-    AQUA("§b"),
-    RED("§c"),
-    LIGHT_PURPLE("§d"),
-    YELLOW("§e"),
-    WHITE("§f"),
-    MINECOIN_GOLD("§g"),
-    MATERIAL_QUARTZ("§h"),
-    MATERIAL_IRON("§i"),
-    MATERIAL_NETHERITE("§j"),
-    MATERIAL_REDSTONE("§m"),
-    MATERIAL_COPPER("§n"),
-    MATERIAL_GOLD("§p"),
-    MATERIAL_EMERALD("§q"),
-    MATERIAL_DIAMOND("§s"),
-    MATERIAL_LAPIS("§t"),
-    MATERIAL_AMETHYST("§u");
+    BLACK("§0"), DARK_BLUE("§1"), DARK_GREEN("§2"), DARK_AQUA("§3"), DARK_RED("§4"), DARK_PURPLE("§5"), GOLD("§6"), GRAY("§7"), DARK_GRAY("§8"), BLUE("§9"), GREEN("§a"), AQUA("§b"), RED("§c"), LIGHT_PURPLE("§d"), YELLOW("§e"), WHITE("§f"), MINECOIN_GOLD("§g"), MATERIAL_QUARTZ("§h"), MATERIAL_IRON("§i"), MATERIAL_NETHERITE("§j"), MATERIAL_REDSTONE("§m"), MATERIAL_COPPER("§n"), MATERIAL_GOLD("§p"), MATERIAL_EMERALD("§q"), MATERIAL_DIAMOND("§s"), MATERIAL_LAPIS("§t"), MATERIAL_AMETHYST("§u"),
+
+    PRIMARY("§b"), SECONDARY("§d"), SUCCESS("§a"), WARN("§e"), ERROR("§c");
 
     private static final String DEFAULT = "§7";
     private static final String DEFAULT_FORM = "§7";
-
-    public static final String PRIMARY = String.valueOf(AQUA);
-    public static final String SECONDARY = String.valueOf(LIGHT_PURPLE);
-    public static final String SUCCESS = String.valueOf(GREEN);
-    public static final String WARN = String.valueOf(YELLOW);
-    public static final String ERROR = String.valueOf(RED);
 
     private final String color;
 
